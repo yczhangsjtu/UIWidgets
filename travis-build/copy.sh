7b8452246f9daa6e44fc01991ec385cdac1c1dd2
Yuncong Zhang <yuncong.zhang@unity3d.com>
2019-03-06 15:09:34 +0800

    Add Travis-CI.

---
+++ travis-build/copy.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+
+PROJECT_PATH=$(pwd)/artifact/$UNITY_PROJECT_PATH
+UNITY_BUILD_DIR=$(pwd)/artifact/Build
+LOG_FILE=$UNITY_BUILD_DIR/unity.log
+
+ERROR_CODE=0
+
+echo "Creating empty project..."
+mkdir -p $PROJECT_PATH
+mkdir -p $UNITY_BUILD_DIR
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile \
+  -createProject "$PROJECT_PATH" \
+  -quit \
+  | tee "$LOG_FILE"
+  
+if [ $? = 0 ] ; then
+  echo "Creating empty project completed successfully."
+  ERROR_CODE=0
+else
+  echo "Creating emtpy project failed. Exited with $?."
+  ERROR_CODE=1
+  echo "Finishing with code $ERROR_CODE"
+  exit $ERROR_CODE
+fi
+
+mkdir -p $PROJECT_PATH/Assets/$PACKAGE_NAME
+cp -r $(pwd)/Editor $(pwd)/Runtime $(pwd)/Tests $(pwd)/scripts $PROJECT_PATH/Assets/$PACKAGE_NAME
+
+echo "Copy package folder to empty project"
+if [ $? = 0 ] ; then
+  echo "Copy completed successfully."
+  ERROR_CODE=0
+else
+  echo "Copy failed. Exited with $?."
+  ERROR_CODE=1
+fi
+
+echo "Finishing with code $ERROR_CODE"
+exit $ERROR_CODE
---
+++ travis-build/run-tests.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+PROJECT_PATH=$(pwd)/artifact/$UNITY_PROJECT_PATH
+UNITY_BUILD_DIR=$(pwd)/artifact/Build
+LOG_FILE=$UNITY_BUILD_DIR/unity.log
+
+ERROR_CODE=0
+
+echo "Running Editor Tests..."
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile \
+  -projectPath "$PROJECT_PATH"\
+  -runEditorTests \
+  | tee "$LOG_FILE"
+
+if [ $? = 0 ] ; then
+  echo "Tests completed successfully."
+  ERROR_CODE=0
+else
+  echo "Tests failed. Exited with $?."
+  ERROR_CODE=1
+fi
+
+echo "Finishing with code $ERROR_CODE"
+exit $ERROR_CODE
